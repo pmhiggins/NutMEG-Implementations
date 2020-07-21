75d4c368db2fe69c8e2faa7c6d09a8d201c0a30b
pmhiggins <pete.matt.higgins@gmail.com>
2020-07-21 14:32:21 +0100

    replot from csvs rather than NutMEG database

--- TOM/EnergyNutrientLimitation.py
+++ TOM/EnergyNutrientLimitation.py
@@ -75,38 +75,73 @@ def get_peffs(other_params={}, MPscale=1, T=300):
 
 
 
-def inbetweens(peffs, ax, rmv=False, stoppers={}, T=300, dt=2000, hatch=None, alph=0.6):
+def inbetweens(peffs, ax, rmv=False, stoppers={}, T=300, dt=2000, hatch=None, alph=0.6, fromcsv=[False, 'x']):
     """Plot growth curves for the three eddiciencies objects in peffs, with a
     fill_between plot between indexes 0 and 2, and a straight line at index 1
     ATP yield of 1 mol per mol CO2.
+
+    This method can be used to run a NutMEG simualtion to predict the growth
+    curves by passing fromcsv's first element as False. The time and population
+    is saved to a csv for convenience, with a preamble given by fromcsv's
+    second element. This can then be loaded in the future for faster plotting.
     """
     collist=['#2c7bb6','#abd9e9', '#fdae61', '#d7191c']
     for ic, peff in enumerate(peffs):
         L,O,S,t,pop= [0,0,0], [0,0,0],[0,0,0],[0,0,0],[0,0,0]
-        for i in range(len(peff)):
-            L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
-            # print(S[i])
-            t2,pop2=[],[]
-            if rmv:
-                es.ecosystem_dbhelper.db_helper.removesimdata(S[i], dbpath=this_dbpath)
+        if not fromcsv[0]:
+            sav = pd.DataFrame({'Time_nATP05':[],'Pop_nATP05':[], 'Time_nATP10':[],'Pop_nATP10':[], 'Time_nATP15':[],'Pop_nATP15':[]})
+            for i in range(len(peff)):
                 L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
-            t2 = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'Time', dbpath=this_dbpath)
-            pop2 = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'no_alive_'+O[i], dbpath=this_dbpath)
-            t[i], pop[i]=[],[]
-            for k, p in zip(t2, pop2):
-                t[i].append(k[0])
-                pop[i].append(p[0])
+                # print(S[i])
+                t2,pop2=[],[]
+                if rmv:
+                    es.ecosystem_dbhelper.db_helper.removesimdata(S[i], dbpath=this_dbpath)
+                    L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
+                t2 = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'Time', dbpath=this_dbpath)
+                pop2 = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'no_alive_'+O[i], dbpath=this_dbpath)
+                t[i], pop[i]=[],[]
+                for k, p in zip(t2, pop2):
+                    t[i].append(k[0])
+                    pop[i].append(p[0])
 
-        print(T, dt)
+            print(T, dt)
 
-        if t[0][-1]<t[2][-1]:
-            for j in range(int((t[2][-1]-t[0][-1])/dt)):
-                t[0].append(t[0][-1]+dt)
-                pop[0].append(pop[0][-1])
+            if t[0][-1]<t[2][-1]:
+                for j in range(int((t[2][-1]-t[0][-1])/dt)):
+                    t[0].append(t[0][-1]+dt)
+                    pop[0].append(pop[0][-1])
+            else:
+                for j in range(int((t[0][-1]-t[2][-1])/dt)):
+                    t[2].append(t[2][-1]+dt)
+                    pop[2].append(pop[2][-1])
+
+            if t[0][-1]<t[1][-1]:
+                for j in range(int((t[1][-1]-t[0][-1])/dt)):
+                    t[0].append(t[0][-1]+dt)
+                    pop[0].append(pop[0][-1])
+                    t[2].append(t[2][-1]+dt)
+                    pop[2].append(pop[2][-1])
+            else:
+                for j in range(int((t[0][-1]-t[1][-1])/dt)):
+                    t[1].append(t[1][-1]+dt)
+                    pop[1].append(pop[1][-1])
+
+            sav['Time_nATP05'] = t[0]
+            sav['Time_nATP10'] = t[1]
+            sav['Time_nATP15'] = t[2]
+            sav['Pop_nATP05'] = pop[0]
+            sav['Pop_nATP10'] = pop[1]
+            sav['Pop_nATP15'] = pop[2]
+            sav.to_csv(fromcsv[1]+'_'+str(ic)+'_'+str(T)+'.csv')
+
         else:
-            for j in range(int((t[0][-1]-t[2][-1])/dt)):
-                t[2].append(t[2][-1]+dt)
-                pop[2].append(pop[2][-1])
+            sav = pd.read_csv(fromcsv[1]+'_'+str(ic)+'_'+str(T)+'.csv')
+            t[0] = sav['Time_nATP05']
+            t[1] = sav['Time_nATP10']
+            t[2] = sav['Time_nATP15']
+            pop[0] = sav['Pop_nATP05']
+            pop[1] = sav['Pop_nATP10']
+            pop[2] = sav['Pop_nATP15']
 
         try:
             if hatch!=None:
@@ -116,7 +151,7 @@ def inbetweens(peffs, ax, rmv=False, stoppers={}, T=300, dt=2000, hatch=None, al
                 ax.plot(t[1],pop[1], c=collist[ic], alpha=alph)
                 ax.fill_between(t[0], pop[0], pop[2], alpha=alph, color=collist[ic])
         except ValueError:
-            inbetweens(peffs, ax, rmv=True, stoppers=stoppers, T=T, dt=dt, alph=alph)
+            inbetweens(peffs, ax, rmv=True, stoppers=stoppers, T=T, dt=dt, alph=alph, fromcsv=fromcsv)
 
 
 
@@ -198,7 +233,11 @@ def Puptakepeffs(T=300):
 
 
 def growthcurves(Ts=[280,300,320], dts=[3000,2000,3000-(40000/25)]):
-    """plot growth curves (manuscript Figure 3)"""
+    """plot growth curves (manuscript Figure 3).
+
+    Simulations have been performed and relevant data saved in
+    data/growthcurves/
+    """
     fig, axs = plt.subplots(nrows=len(Ts), ncols=4, figsize=(9,8), sharey=True)
 
 
@@ -207,19 +246,19 @@ def growthcurves(Ts=[280,300,320], dts=[3000,2000,3000-(40000/25)]):
 
         ############################## CO2 CONCENTRATION
         peffs = CO2peffs(T=int(Ts[i]))
-        inbetweens(peffs, axs[i][0], T=int(Ts[i]), dt=int(dts[i]), rmv=rmv, alph=al)
+        inbetweens(peffs, axs[i][0], T=int(Ts[i]), dt=int(dts[i]), rmv=rmv, alph=al, fromcsv=[True,'data/growthcurves/CO2'])
 
         ############################## H2 CONCENTRATION
         peffs = H2peffs(T=int(Ts[i]))
-        inbetweens(peffs, axs[i][1], T=int(Ts[i]), dt=int(dts[i]), rmv=rmv, alph=al)
+        inbetweens(peffs, axs[i][1], T=int(Ts[i]), dt=int(dts[i]), rmv=rmv, alph=al, fromcsv=[True,'data/growthcurves/H2'])
 
         ############################## P CONCENTRATION
         peffs = Ppeffs(T=int(Ts[i]))
-        inbetweens(peffs, axs[i][2], T=int(Ts[i]), dt=int(dts[i]), rmv=rmv, alph=al)
+        inbetweens(peffs, axs[i][2], T=int(Ts[i]), dt=int(dts[i]), rmv=rmv, alph=al, fromcsv=[True,'data/growthcurves/P'])
 
         ############################## P UPTAKE
         peffs = Puptakepeffs(T=int(Ts[i]))
-        inbetweens(peffs, axs[i][3], rmv=rmv, T=int(Ts[i]), dt=int(dts[i]), alph=al)
+        inbetweens(peffs, axs[i][3], rmv=rmv, T=int(Ts[i]), dt=int(dts[i]), alph=al, fromcsv=[True,'data/growthcurves/k_P'])
 
         # axs[0][i].set_title('T: '+str(int(Ts[i]))+' K')
     axs[0][0].set_title('[$\mathregular{CO}_{2}$]')
@@ -257,119 +296,87 @@ def growthcurves(Ts=[280,300,320], dts=[3000,2000,3000-(40000/25)]):
     plt.savefig('growthcurves.pdf')
     plt.show()
 
-growthcurves()
 
 
-def plot_Trange(energypeffs, nutrientpeffs, axs, rmv=False, stoppers={}, Tvals=range(280,331), hatch=None):
+
+def plot_Trange(limiterpeffs, colno, axs, rmv=False, stoppers={}, Tvals=range(280,331), fromcsv=[False, 'y']):
     """Function for plotting Figure 4.
 
     Plots one list of energy limitations and one of nutrient limitations at a
     time, passed in the form of efficiencies objects, energypeffs or nutrientpeffs.
     Pass rmv as True to delete results and perform simulations again.
 
-    In a previous version the hatch option was used to hatch some of the plots,
-    now it is used to plot the seperate axes in multiple calls to this funcion.
+    This method can be used to run a NutMEG simualtion to predict the growth
+    curves by passing fromcsv's first element as False. The time and population
+    is saved to a csv for convenience, with a preamble given by fromcsv's
+    second element. This can then be loaded in the future for faster plotting.
+
     """
     collist=['#2c7bb6','#abd9e9', '#fdae61', '#d7191c']
-    energydf = pd.DataFrame(energypeffs)
-    nutrientdf = pd.DataFrame(nutrientpeffs)
-    print(energydf)
-    for ic in range(len(energydf.columns)):
-        # choose a good timestep. Helps with the fill_between.
-        dt =3000-((num)*(1000/25))
-        if T==300:
-            dt=2000
-        if T==325:
-            dt=1000
+    limiterdf = pd.DataFrame(limiterpeffs)
+    # nutrientdf = pd.DataFrame(nutrientpeffs)
+    print(limiterdf)
+
+    for ic in range(len(limiterdf.columns)):
 
         GR = [[],[],[]]
         BM = [[],[],[]]
         BS = [[],[],[]]
 
-        for peff in energydf[ic]:
+        if fromcsv[0]==False:
+            # perfrom simulations and/or look in the database for results
+            for peff in limiterdf[ic]:
+            # loops through temperature values
 
-            L,O,S,t,pop= [0,0,0], [0,0,0],[0,0,0],[0,0,0],[0,0,0]
-            for i in range(len(peff)):
-                L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
-                if rmv:
-                    es.ecosystem_dbhelper.db_helper.removesimdata(S[i], dbpath=this_dbpath)
+                # choose a good timestep. Helps with the fill_between.
+                dt =3000-((Tvals[ic]-280)*(1000/25))
+                if Tvals[ic]==300:
+                    dt=2000
+                if Tvals[ic]==325:
+                    dt=1000
+
+                L,O,S,t,pop= [0,0,0], [0,0,0],[0,0,0],[0,0,0],[0,0,0]
+
+                for i in range(len(peff)):
                     L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
+                    if rmv:
+                        es.ecosystem_dbhelper.db_helper.removesimdata(S[i], dbpath=this_dbpath)
+                        L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
 
-                gr = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'PeakGR', OrgID='', dbpath=this_dbpath)
-                gr = ast.literal_eval(gr)[0]
-                GR[i].append(gr*3600)
-                bm = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'FinBM_cells_tot', OrgID='', dbpath=this_dbpath)
-                BM[i].append(bm)
-                bs = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'Composition', OrgID='', dbpath=this_dbpath)
-                bs = ast.literal_eval(tuple(bs)[-1][0])['CH4(g)']
-                BS[i].append(bs)
+                    gr = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'PeakGR', OrgID='', dbpath=this_dbpath)
+                    gr = ast.literal_eval(gr)[0]
+                    GR[i].append(gr*3600)
+                    bm = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'FinBM_cells_tot', OrgID='', dbpath=this_dbpath)
+                    BM[i].append(bm)
+                    bs = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'Composition', OrgID='', dbpath=this_dbpath)
+                    bs = ast.literal_eval(tuple(bs)[-1][0])['CH4(g)']
+                    BS[i].append(bs)
 
+            sav = pd.DataFrame({'Temp':Tvals,'GrowthRate_nATP05':GR[0],
+              'FinalBiomass_nATP05':BM[0],'FinalCH4_nATP05':BS[0],
+              'GrowthRate_nATP10':GR[1], 'FinalBiomass_nATP10':BM[1],
+              'FinalCH4_nATP10':BS[0], 'GrowthRate_nATP15':GR[2],
+              'FinalBiomass_nATP15':BM[2],'FinalCH4_nATP15':BS[2]})
+            sav.to_csv(fromcsv[1]+'_'+str(ic)+'.csv')
 
-        if hatch!=None:
-            axs[0][1].plot(Tvals,GR[1], c=collist[ic], alpha=0.75)#, ls='--')
-            axs[1][1].plot(Tvals,BM[1], c=collist[ic], alpha=0.75)#, ls='--')
-            axs[2][1].plot(Tvals,BS[1], c=collist[ic], alpha=0.75)#, ls='--')
-
-            axs[0][1].fill_between(Tvals, GR[0], GR[2], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
-            axs[1][1].fill_between(Tvals, BM[0], BM[2], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
-            axs[2][1].fill_between(Tvals, BS[0], BS[2], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
-
-        else:
-            axs[0][0].plot(Tvals,GR[1], c=collist[ic], alpha=0.75)
-            axs[1][0].plot(Tvals,BM[1], c=collist[ic], alpha=0.75)
-            axs[2][0].plot(Tvals,BS[1], c=collist[ic], alpha=0.75)
-
-            axs[0][0].fill_between(Tvals, GR[0], GR[2], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
-            axs[1][0].fill_between(Tvals, BM[0], BM[2], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
-            axs[2][0].fill_between(Tvals, BS[0], BS[2], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
-
-    for ic in range(len(nutrientdf.columns)):
-
-        GR = [[],[],[]]
-        BM = [[],[],[]]
-        BS = [[],[],[]]
-
-        for peff in nutrientdf[ic]:
-
-            L,O,S,t,pop= [0,0,0], [0,0,0],[0,0,0],[0,0,0],[0,0,0]
-            for i in range(len(peff)):
-                L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
-                if rmv:
-                    es.ecosystem_dbhelper.db_helper.removesimdata(S[i], dbpath=this_dbpath)
-                    L[i], O[i], S[i] = peff[i].perform_sim(dbpath=this_dbpath, dt=dt, stoppers=stoppers)
-
-                gr = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'PeakGR', OrgID='', dbpath=this_dbpath)
-                gr = ast.literal_eval(gr)[0]
-                GR[i].append(gr*3600)
-                bm = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'FinBM_cells_tot', OrgID='', dbpath=this_dbpath)
-                BM[i].append(bm)
-                bs = es.ecosystem_dbhelper.db_helper.extract_param_db_Sim(S[i], 'Composition', OrgID='', dbpath=this_dbpath)
-                bs = ast.literal_eval(tuple(bs)[-1][0])['CH4(g)']
-                BS[i].append(bs)
+        sav = pd.read_csv(fromcsv[1]+'_'+str(ic)+'.csv')
 
 
-        if hatch!=None:
-            axs[0][3].plot(Tvals,GR[1], c=collist[ic], alpha=0.6)#, ls='--')
-            axs[1][3].plot(Tvals,BM[1], c=collist[ic], alpha=0.6)#, ls='--')
-            axs[2][3].plot(Tvals,BS[1], c=collist[ic], alpha=0.6)#, ls='--')
+        axs[0][colno].plot(Tvals,sav['GrowthRate_nATP10'], c=collist[ic], alpha=0.75)#, ls='--')
+        axs[1][colno].plot(Tvals,sav['FinalBiomass_nATP10'], c=collist[ic], alpha=0.75)#, ls='--')
+        axs[2][colno].plot(Tvals,sav['FinalCH4_nATP10'], c=collist[ic], alpha=0.75)#, ls='--')
 
-            axs[0][3].fill_between(Tvals, GR[0], GR[2], alpha=0.6, edgecolor=collist[ic], facecolor=collist[ic])#, facecolor='none'), hatch=hatch)
-            axs[1][3].fill_between(Tvals, BM[0], BM[2], alpha=0.6, edgecolor=collist[ic], facecolor=collist[ic])#, facecolor='none'), hatch=hatch)
-            axs[2][3].fill_between(Tvals, BS[0], BS[2], alpha=0.6, edgecolor=collist[ic], facecolor=collist[ic])#, facecolor='none'), hatch=hatch)
+        axs[0][colno].fill_between(Tvals, sav['GrowthRate_nATP05'], sav['GrowthRate_nATP15'], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
+        axs[1][colno].fill_between(Tvals, sav['FinalBiomass_nATP05'], sav['FinalBiomass_nATP15'], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
+        axs[2][colno].fill_between(Tvals, sav['FinalCH4_nATP05'], sav['FinalCH4_nATP15'], alpha=0.75, edgecolor=collist[ic], facecolor=collist[ic])
 
-        else:
-            axs[0][2].plot(Tvals,GR[1], c=collist[ic], alpha=0.6)
-            axs[1][2].plot(Tvals,BM[1], c=collist[ic], alpha=0.6)
-            axs[2][2].plot(Tvals,BS[1], c=collist[ic], alpha=0.6)
-
-            axs[0][2].fill_between(Tvals, GR[0], GR[2], alpha=0.6, edgecolor=collist[ic], facecolor=collist[ic])
-            axs[1][2].fill_between(Tvals, BM[0], BM[2], alpha=0.6, edgecolor=collist[ic], facecolor=collist[ic])
-            axs[2][2].fill_between(Tvals, BS[0], BS[2], alpha=0.6, edgecolor=collist[ic], facecolor=collist[ic])
 
 
 
 def grbmbs(Tvals=range(280,331)):
-    """Function for the final plot which became Figure 4 in the manuscript. """
+    """Function for the final plot which became Figure 4 in the manuscript.
+    Simulations have been performed and relevant output data saved in
+    data/tempcurves"""
 
     fig, axs = plt.subplots(nrows=3, ncols=4, figsize=(9,8), sharey='row')
 
@@ -378,6 +385,7 @@ def grbmbs(Tvals=range(280,331)):
     _h2peffs=[]
     _Ppeffs=[]
     _Puptakepeffs=[]
+    print('loading peffs...')
     for T in Tvals:
         _co2peffs.append(CO2peffs(T=T))
         _h2peffs.append(H2peffs(T=T))
@@ -385,8 +393,14 @@ def grbmbs(Tvals=range(280,331)):
         _Puptakepeffs.append(Puptakepeffs(T=T))
 
     # plot the growth rates, biomass and methane
-    plot_Trange(_co2peffs, _Ppeffs, axs, Tvals=Tvals)#, dt=3000)
-    plot_Trange(_h2peffs, _Puptakepeffs, axs, Tvals=Tvals, hatch='x')#, dt=3000)
+    # plot_Trange(_co2peffs, _Ppeffs, axs, Tvals=Tvals)#, dt=3000)
+    plot_Trange(_co2peffs,0, axs, Tvals=Tvals, fromcsv=[True, 'data/tempcurves/CO2'])#, dt=3000)
+    plot_Trange(_h2peffs,1, axs, Tvals=Tvals, fromcsv=[True, 'data/tempcurves/H2'])#, dt=3000)
+    plot_Trange(_Ppeffs,2, axs, Tvals=Tvals, fromcsv=[True, 'data/tempcurves/P'])#, dt=3000)
+    plot_Trange(_Puptakepeffs,3, axs, Tvals=Tvals, fromcsv=[True, 'data/tempcurves/kP'])#, dt=3000)
+
+
+    # plot_Trange(_h2peffs, _Puptakepeffs, axs, Tvals=Tvals, hatch='x')#, dt=3000)
 
     # tidy up the plot
     axs[0][0].set_ylabel('Peak Growth Rate [$\mathregular{hr}^{-1}$]')
@@ -408,6 +422,7 @@ def grbmbs(Tvals=range(280,331)):
             if j!=0:
                 a.tick_params(labelcolor='none', axis='y')
             a.set_yscale('log')
+            axs[0][j].set_ylim(1e-3, 2e-1)
 
     axs[2][0].legend([n+' Optimal [$\mathregular{CO}_{2}$]' for n in ['', '-1%','-10%','-40%']], loc='upper center', bbox_to_anchor=(0.5, -0.2))
     axs[2][1].legend([n+' Optimal [$\mathregular{H}_{2}$]' for n in ['', '-1%','-5%','-20%']], loc='upper center', bbox_to_anchor=(0.5, -0.2))
@@ -419,7 +434,11 @@ def grbmbs(Tvals=range(280,331)):
     plt.savefig('grbmbs.pdf')
     plt.show()
 
-# grbmbs()
+
+
+
+growthcurves()
+grbmbs()
 
 
 
